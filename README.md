f977859e127e9a6c8ce6dc0d18911f0d2feedc3c
DmitryKirilyuk <d.kiriliuk>
2015-01-30 13:43:04 +0300

    download zip from ipgeobase.ru

--- README.md
+++ README.md
@@ -10,10 +10,17 @@
 
 To create GeoIP `GeoLiteCity-Blocks.csv` and `GeoLiteCity-Location.csv` files run command:
 
-    geodbconverter --ipblocks <path to cidr_optim.txt>
-                   --cities <path to cities.txt>
-                   <output directory path>
+    geodbconverter [options] [outputDir]
 
-File `cities.txt` should be encoded in UTF-8.
+Options:
+
+    -h, --help                output usage information
+    -V, --version             output the version number
+    -d, --download-url [url]  ipgeobase.ru zip archive url (by default http://ipgeobase.ru/files/db/Main/geo_files.zip)
+    -l, --local               Use local files without archive downloading
+    -i, --ipblocks [path]     Used only with --cities option. It's path to local file with blocks of IP addresses if used with --local option. It's file name in archive if DB is downloaded from ipgeobase.ru. By default "cidr_optim.txt"
+    -c, --cities [path]       Used only with --ipblocks option. It's path to local file with cities names if used with --local option. It's file name in archive if DB is downloaded from ipgeobase.ru. By default "cities.txt"
+    --ipblocks-output [path]  IP blocks output file name (by default "GeoLiteCity-Blocks.csv")
+    --cities-output [path]    Cities output file name (by default "GeoLiteCity-Location.csv")
 
 To convert `.csv` files to `.dat` use [mteodoro/mmutils](https://github.com/mteodoro/mmutils).
--- bin/geodbconverter.js
+++ bin/geodbconverter.js
@@ -3,31 +3,95 @@
 var program = require('commander');
 var package = require('../package.json');
 var lib     = require('../lib');
+var q       = require('q');
 
 program
     .version(package.version)
-    .usage('[options] <outputDir>')
+    .usage('[options] [outputDir]')
     .description(package.description)
-    .option('-i, --ipblocks <path>', 'Path to cidr_optim.txt (blocks of IP addresses)')
-    .option('-c, --cities <path>', 'Path to cities.txt (cities names)')
+
+    .option(
+        '-d, --download-url [url]',
+        'ipgeobase.ru zip archive url (by default http://ipgeobase.ru/files/db/Main/geo_files.zip)',
+        'http://ipgeobase.ru/files/db/Main/geo_files.zip'
+    )
+
+    .option('-l, --local', 'Use local files without archive downloading')
+
+    .option(
+        '-i, --ipblocks [path]',
+        'Used only with --cities option. ' +
+        "It's path to local file with blocks of IP addresses if used with --local option. " +
+        "It's file name in archive if DB is downloaded from ipgeobase.ru. " +
+        'By default "cidr_optim.txt"',
+        'cidr_optim.txt'
+    )
+    .option(
+        '-c, --cities [path]',
+        'Used only with --ipblocks option. ' +
+        "It's path to local file with cities names if used with --local option. " +
+        "It's file name in archive if DB is downloaded from ipgeobase.ru. " +
+        'By default "cities.txt"',
+        'cities.txt'
+    )
+
+    .option(
+        '--ipblocks-output [path]',
+        'IP blocks output file name (by default "GeoLiteCity-Blocks.csv")',
+        'GeoLiteCity-Blocks.csv'
+    )
+    .option(
+        '--cities-output [path]',
+        'Cities output file name (by default "GeoLiteCity-Location.csv")',
+        'GeoLiteCity-Location.csv'
+    )
+
     .parse(process.argv);
 
-if (!program.ipblocks || !program.cities) {
-    console.error('"ipblocks" and "cities" options must be set. See --help');
-    process.exit(1);
-}
+main();
 
-var outputDir = program.args.shift();
-if (!outputDir) {
-    console.error('Output directory does not set. See --help');
-    process.exit(1);
-}
+function main() {
+    var conversionOptions = {
+        outputDirPath:          program.args.shift() || './',
+        ipBlocksOutputFileName: program.ipblocksOutput,
+        citiesOutputFileName:   program.citiesOutput
+    };
 
-lib.convert(program.ipblocks, program.cities, outputDir, function(err) {
-    if (err) {
-        console.error(err);
-        process.exit(1);
+    var promise = q.when();
+
+    if (program.local) {
+        conversionOptions.ipBlocksInputFilePath = program.ipblocks;
+        conversionOptions.citiesInputFilePath   = program.cities;
+    } else {
+        console.log('Start downloading...');
+
+        promise = lib.download({
+            downloadUrl:      program.downloadUrl,
+            ipBlocksFileName: program.ipblocks,
+            citiesFileName:   program.cities
+        })
+            .then(function(filesPaths) {
+                console.log('Downloading completed');
+                
+                conversionOptions.ipBlocksInputFilePath = filesPaths.ipBlocksFilePath;
+                conversionOptions.citiesInputFilePath   = filesPaths.citiesFilePath;
+            })
+            .catch(function(err) {
+                console.error('Downloading failed');
+                process.exit(1);
+            });
     }
 
-    console.log('Done!');
-});
+    promise.then(function() {
+        console.log('Start conversion...');
+
+        lib.convert(conversionOptions)
+            .then(function() {
+                console.log('Conversion completed');
+            })
+            .catch(function(err) {
+                console.error('Conversion failed', err);
+                process.exit(1);
+            });
+    });
+}
